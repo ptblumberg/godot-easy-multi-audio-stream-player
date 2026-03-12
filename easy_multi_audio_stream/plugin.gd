2f43131b1eb8470354da2165c9de9240721f191b
ptblumberg <78135813+ptblumberg@users.noreply.github.com>
2026-03-12 00:13:52 -0600

    Updated Icon and Plugin CFG

--- easy_multi_audio_stream/plugin.gd
+++ easy_multi_audio_stream/plugin.gd
@@ -6,20 +6,22 @@ func _enter_tree() -> void:
 		"EasyMultiAudioStreamPlayer",
 		"AudioStreamPlayer",
 		preload("res://addons/easy_multi_audio_stream/easy_multi_audio_stream_player.gd"),
-		preload("res://addons/easy_multi_audio_stream/easymultipleicon.png")
+		preload("res://addons/easy_multi_audio_stream/easymultipleicon2.png")
 	)
 	add_custom_type(
 		"EasyMultiAudioStreamPlayer2D",
 		"AudioStreamPlayer2D",
 		preload("res://addons/easy_multi_audio_stream/easy_multi_audio_stream_player.gd"),
-		preload("res://addons/easy_multi_audio_stream/easymultipleicon.png")
+		preload("res://addons/easy_multi_audio_stream/easymultipleicon2.png")
 	)
 	add_custom_type(
 		"EasyMultiAudioStreamPlayer3D",
 		"AudioStreamPlayer3D",
 		preload("res://addons/easy_multi_audio_stream/easy_multi_audio_stream_player.gd"),
-		preload("res://addons/easy_multi_audio_stream/easymultipleicon.png")
+		preload("res://addons/easy_multi_audio_stream/easymultipleicon2.png")
 	)
 
 func _exit_tree() -> void:
 	remove_custom_type("EasyMultiAudioStreamPlayer")
+	remove_custom_type("EasyMultiAudioStreamPlayer2D")
+	remove_custom_type("EasyMultiAudioStreamPlayer3D")
